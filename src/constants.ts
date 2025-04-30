0f39c3e7f3bf3f0aaab5e0613085d5f236de1bb5
arashmogh <arash.ms1994@gmail.com>
2025-04-30 19:35:54 +0330

    Add Some Data for mock

---
+++ src/constants.ts
@@ -0,0 +1,22 @@
+export const TOPBLOGS = {
+  frame1: {
+    Image: "/TopBLogPics/Frame 20.jpg",
+    Title: "Top blog",
+    Des: "CodeStorm 2025: Riding the Digital Surge of Programming Innovation!",
+  },
+  frame2: {
+    Image: "/TopBLogPics/Frame 21.jpg",
+    Title: "Top blog",
+    Des: "CodeStorm 2025: Riding the Digital Surge of Programming Innovation!",
+  },
+  frame3: {
+    Image: "/TopBLogPics/Frame 22.jpg",
+    Title: "Top blog",
+    Des: "CodeStorm 2025: Riding the Digital Surge of Programming Innovation!",
+  },
+  frame4: {
+    Image: "/TopBLogPics/Frame 23.jpg",
+    Title: "Top blog",
+    Des: "CodeStorm 2025: Riding the Digital Surge of Programming Innovation!",
+  },
+};
