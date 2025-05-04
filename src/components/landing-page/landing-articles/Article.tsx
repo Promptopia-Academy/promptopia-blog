f75dadcb88d405ebad7d2a15a39a6fdf5a5a4d4c
arashmogh <arash.ms1994@gmail.com>
2025-05-05 00:34:37 +0330

    Feat: Add some changes for hover and transition

--- src/components/landing-page/landing-articles/Article.tsx
+++ src/components/landing-page/landing-articles/Article.tsx
@@ -11,12 +11,12 @@ const Article: React.FC<IArticleProps> = ({ Des, Images }) => {
   return (
     <Link href="#">
       <div
-        className="relative w-full h-full rounded-xl overflow-hidden bg-cover bg-center text-white"
+        className="relative w-full h-full rounded-[12px] overflow-hidden bg-cover bg-center text-white hover:scale-97 duration-300 transition-all"
         style={{ backgroundImage: `url(${Images})` }}
       >
         <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col justify-between p-4">
           <h2 className="text-3xl font-light">{Des}</h2>
-          <button className="self-end bg-[#3F8EFC] text-white px-4 py-2 rounded-[12px] text-base font-light w-[120px] h-8">
+          <button className="self-end bg-[#3F8EFC] text-white rounded-[12px] text-base font-light w-[120px] h-8">
             Read more...
           </button>
         </div>
